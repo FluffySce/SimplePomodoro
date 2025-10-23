5f7b6201fadfa89d7ec9d89d70fb0e48eacbb5d3
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2025-10-23 20:42:26 +0530

    bare-bones web pomodoro working

--- script.js
+++ script.js
@@ -15,9 +15,51 @@ const timer = setInterval(() => {
 }, 1000);
 
 */
-let timer = {
-  pomodor: 25,
-  shortBreak: 5,
-  longBreak: 15,
-  longBreakInterval: 4,
+
+//const { session, app } = require("electron");
+
+const bell = new Audio("./sounds/bell.wav");
+const startBtn = document.querySelector(".btn-start");
+const timer = document.querySelector(".mins");
+let myInterval;
+//sessions has started or not is determined by below flag
+let state = true;
+
+const appTimer = () => {
+  const sessionTime = Number.parseInt(timer.textContent);
+
+  if (state) {
+    state = false;
+    let totalSeconds = sessionTime * 60;
+    /* const updateSecs = () => {
+            //pass
+        } */
+    myInterval = setInterval(() => {
+      //updating seconds
+
+      const minsDiv = document.querySelector(".mins");
+      const secsDiv = document.querySelector(".secs");
+
+      totalSeconds--;
+
+      let minsLeft = Math.floor(totalSeconds / 60);
+      let secsLeft = totalSeconds % 60;
+
+      if (secsLeft < 10) {
+        secsDiv.textContent = "0" + secsLeft;
+      } else {
+        secsDiv.textContent = secsLeft;
+      }
+      minsDiv.textContent = `${minsLeft}`;
+
+      if (minsLeft === 0 && secsLeft === 0) {
+        bell.play();
+        clearInterval(myInterval);
+      }
+    }, 1000);
+  } else {
+    alert("Session has already started!");
+  }
 };
+
+startBtn.addEventListener("click", appTimer);
