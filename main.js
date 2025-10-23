ab4dbed38d4c78b0b10a7c34302a1798f7bad9b3
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2025-10-23 19:55:47 +0530

    basic window functionality

--- main.js
+++ main.js
@@ -9,10 +9,12 @@ const createWindow = () => {
   win.loadFile("index.html");
 };
 
+// app.on is not used as whenReady is preferred acc to elec docs due to some fallback (gotta read)
 app.whenReady().then(() => {
   createWindow();
 });
 
+// this is to check if the window is closed if it is clsoe the app too
 app.on("window-all-closed", () => {
   if (process.platform != "darwin") app.quit();
 });
